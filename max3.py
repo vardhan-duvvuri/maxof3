06b2d3070cd45723ce8103c9258c026421f0e2ab
vardhan-duvvuri <vardhan.duvvuri@outlook.com>
2013-06-14 10:17:25 +0530

    first commit

---
+++ max3.py
@@ -0,0 +1,10 @@
+a,b,c=None,None,None
+a=int(raw_input("Enter a value"))
+b=int(raw_input("Enter b value"))
+c=int(raw_input("Enter c value"))
+if a>b and a>c:
+    print "%d is big" %a
+elif b>c:
+    print "%d is big" %b
+else:
+    print "%d is big" %c
